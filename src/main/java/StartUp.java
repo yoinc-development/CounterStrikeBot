5f08504007faea00e3f2d5a1d0e13ab9f352f291
Janes Thomas <contact@aatha.ch>
2023-06-18 06:20:54 +0200

    added homepage as playing activity

--- src/main/java/StartUp.java
+++ src/main/java/StartUp.java
@@ -1,5 +1,6 @@
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.events.GenericEvent;
 import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.hooks.EventListener;
@@ -23,6 +24,7 @@ public class StartUp implements EventListener {
                     .addEventListeners(new RetakeMessage(properties))
                     .build();
 
+            jda.getPresence().setActivity(Activity.playing("YOINC.ch"));
             jda.awaitReady();
         } catch (LoginException ex) {
             System.out.println("Nice. Login failed.");
