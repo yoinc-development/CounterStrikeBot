e0a54d0f63bcb073cf409b71911d3f11df37377c
Janes Thomas <janes.thomas@emea.merkleinc.com>
2023-07-21 16:33:53 +0200

    continued implementation of ftp file reader

--- src/main/java/ConsoleUpdate.java
+++ src/main/java/ConsoleUpdate.java
@@ -2,7 +2,7 @@ import net.dv8tion.jda.api.entities.MessageChannel;
 import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPFile;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.SocketException;
 import java.util.Properties;
 
@@ -14,6 +14,8 @@ public class ConsoleUpdate implements Runnable{
     private String serverFtpPassword;
 
     private MessageChannel channel;
+
+    final String MARKED_END = "--- END ---";
 
     public ConsoleUpdate(Properties properties, MessageChannel channel) {
         serverFtpIp = properties.getProperty("server.ftp.ip");
@@ -34,14 +36,39 @@ public class ConsoleUpdate implements Runnable{
 
             FTPFile[] ftpFileArray = ftp.listFiles("console.log");
 
-            /*
-            TODO: implement reading process of ftpFileArray
-            - ftpFileArray.length should only be one since in the current directoy
-            only one file named "console.log" should exist.
-            this file has to be read for a certain amount of time to await the key
-            message "The terrorists have won 10 rounds in a row!"
+            InputStream inputStream = ftp.retrieveFileStream(ftpFileArray[0].getName());
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            String lastLine = "";
+            while (reader.ready()) {
+                lastLine = reader.readLine();
+            }
+
+            if(!MARKED_END.equals(lastLine)) {
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+                int lineNumber = 1;
+                int lastLineNumber = 1;
+                while (reader.ready()) {
+                    if(MARKED_END.equals(reader.readLine())) {
+                        lastLineNumber = lineNumber;
+                    }
+                    lineNumber++;
+                }
+
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+                reader.skip(lastLineNumber);
+                while (reader.ready()) {
+                    //check for streak message
+                }
+                //update file with MARKED_END and upload it again on the server
+                //ftp.storeFile("console.log", inputStream);
+            }
+
+
+           /*
             TODO: send congratulatory message through variable "channel"
-             */
+            */
 
         } catch (SocketException ex) {
             System.out.println("Socket Exception");
