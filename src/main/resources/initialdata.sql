21b90e173d988e3149e6564a95bb1217928a9950
Janes Thomas <work@yoinc.ch>
2024-06-11 22:08:57 +0200

    added intialdata.sql in case table has to be dropped

---
+++ src/main/resources/initialdata.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS wow (wow_id int PRIMARY KEY AUTO_INCREMENT, username varchar(50) NOT NULL UNIQUE, url varchar(200) NOT NULL);
+
+INSERT INTO wow(username, url)
+VALUES("jay_th", "https://cdn.discordapp.com/attachments/449281855175393280/1221510017354563674/loud.mov");
+INSERT INTO wow(username, url)
+VALUES("vi24ra", "https://cdn.discordapp.com/attachments/288367861515419649/1167948820525621248/Dropshot.mp4");
+INSERT INTO wow(username, url)
+VALUES("aatha", "https://cdn.discordapp.com/attachments/844510835241910303/1225082807110336653/Me_Is_Sorry_Janes.mp4");
+INSERT INTO wow(username, url)
+VALUES("CSBot", "https://www.youtube.com/watch?v=2qTHmSyqrok");
+INSERT INTO wow(username, url)
+VALUES("korunde", "https://www.youtube.com/watch?v=Ygjd-t8btgY");
