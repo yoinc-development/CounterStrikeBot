72e4c1c08de970665639bc57f912ab2f2c310ebe
Janes Thomas <work@yoinc.ch>
2024-11-18 10:23:05 +0100

    fixed DataService connection issues

--- src/main/java/services/DataService.java
+++ src/main/java/services/DataService.java
@@ -16,52 +16,66 @@ public class DataService {
 
     public DataService(Properties properties) throws SQLException {
         this.properties = properties;
-        connection = DriverManager.getConnection(properties.getProperty("db.url"));
     }
 
     public String getDiscordIdForUsername(String username) throws SQLException {
+        connection = DriverManager.getConnection(properties.getProperty("db.url"));
+
         PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM users WHERE username = ?");
         preparedStatement.setString(1, username);
         ResultSet resultSet = preparedStatement.executeQuery();
 
         while (resultSet.next()) {
-            return resultSet.getString("discordID");
+            String result = resultSet.getString("discordID");
+            connection.close();
+            return result;
         }
+        connection.close();
         return null;
     }
 
     public String getSteamIDForDiscordID(String discordID) throws SQLException {
+        connection = DriverManager.getConnection(properties.getProperty("db.url"));
         PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM users WHERE discordID = ?");
         preparedStatement.setString(1, discordID);
         ResultSet resultSet = preparedStatement.executeQuery();
 
         while (resultSet.next()) {
-            return resultSet.getString("steamID");
+            String result = resultSet.getString("steamID");
+            connection.close();
+            return result;
         }
+        connection.close();
         return null;
     }
 
     public void addWowEvent(String discordID, String url) throws SQLException {
+        connection = DriverManager.getConnection(properties.getProperty("db.url"));
         PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO wow(f_user_id, url) VALUES(?,?)");
         preparedStatement.setInt(1, getUserIDForDiscordID(discordID));
         preparedStatement.setString(2, url);
         preparedStatement.executeUpdate();
+        connection.close();
     }
 
     public void updateWowEvent(String discordID, String url) throws SQLException {
+        connection = DriverManager.getConnection(properties.getProperty("db.url"));
         PreparedStatement preparedStatement = connection.prepareStatement("UPDATE wow SET url = ? WHERE f_user_id = ?");
         preparedStatement.setString(1, url);
         preparedStatement.setInt(2, getUserIDForDiscordID(discordID));
         preparedStatement.executeUpdate();
+        connection.close();
     }
 
     public void addUserToDatabase(String username, String discordID) {
         try {
+            connection = DriverManager.getConnection(properties.getProperty("db.url"));
             if (!isDiscordIdInDatabase(discordID)) {
                 PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO users(username, discordID) VALUES(?,?)");
                 preparedStatement.setString(1, username);
                 preparedStatement.setString(2, discordID);
                 preparedStatement.executeUpdate();
+                connection.close();
             }
         } catch (SQLException ex) {
             System.out.println("[CSBot - DataService - " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd.MM.yyyy - HH:mm:ss")) + "] Can't add " + username + " with discordID " + discordID);
@@ -91,6 +105,7 @@ public class DataService {
     }
 
     public HashMap<String, String> getAllWowEntries() throws SQLException {
+        connection = DriverManager.getConnection(properties.getProperty("db.url"));
         HashMap<String, String> returnMap = new HashMap<String, String>();
         PreparedStatement preparedStatement = connection.prepareStatement("SELECT u.discordID, w.url FROM wow AS w LEFT JOIN users AS u ON w.f_user_id = u.user_id");
         ResultSet resultSet = preparedStatement.executeQuery();
@@ -98,6 +113,7 @@ public class DataService {
         while (resultSet.next()) {
             returnMap.put(resultSet.getString("u.discordID"), resultSet.getString("w.url"));
         }
+        connection.close();
         return returnMap;
     }
 
@@ -142,43 +158,54 @@ public class DataService {
     }
 
     public boolean hasSentRetakeInvite() throws SQLException {
+        connection = DriverManager.getConnection(properties.getProperty("db.url"));
         PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM retake_watchdog");
         ResultSet resultSet = preparedStatement.executeQuery();
-        return resultSet.next() && resultSet.getBoolean("has_sent_invite");
+        boolean hasSent = resultSet.next() && resultSet.getBoolean("has_sent_invite");
+        connection.close();
+        return hasSent;
     }
 
     public String getRetakeInviteMsgId() throws SQLException {
+        connection = DriverManager.getConnection(properties.getProperty("db.url"));
         PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM retake_watchdog WHERE has_sent_invite = 1");
         ResultSet resultSet = preparedStatement.executeQuery();
-        return resultSet.next() ? resultSet.getString("msg_id") : "";
+        String resultId = resultSet.next() ? resultSet.getString("msg_id") : "";
+        connection.close();
+        return resultId;
     }
 
     public void addRetakeInvite(String msg_id, String timestamp) throws SQLException {
+        connection = DriverManager.getConnection(properties.getProperty("db.url"));
         PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO retake_watchdog VALUES (?,?,1)");
         preparedStatement.setString(1, msg_id);
         preparedStatement.setString(2, timestamp);
         preparedStatement.executeUpdate();
+        connection.close();
     }
 
     public void removeRetakeInvite() throws SQLException {
+        connection = DriverManager.getConnection(properties.getProperty("db.url"));
         PreparedStatement preparedStatement = connection.prepareStatement("SELECT COUNT(*) FROM retake_watchdog");
         ResultSet resultSet = preparedStatement.executeQuery();
 
         if (resultSet.next() && resultSet.getInt(1) != 0) {
             preparedStatement = connection.prepareStatement("DELETE FROM retake_watchdog WHERE has_sent_invite = 1");
             preparedStatement.executeUpdate();
+            connection.close();
         }
     }
 
     public RetakePlayer getHighestRetakeScoreAndPlayer() throws SQLException {
+        connection = DriverManager.getConnection(properties.getProperty("db.url"));
         RetakePlayer retakePlayer = null;
-
         PreparedStatement preparedStatement = connection.prepareStatement("SELECT l.name, l.value FROM lvl_base as l WHERE l.value in (SELECT MAX(value) FROM lvl_base)");
         ResultSet resultSet = preparedStatement.executeQuery();
 
         while (resultSet.next()) {
             retakePlayer = new RetakePlayer(resultSet.getString("l.name"), resultSet.getInt("l.value"));
         }
+        connection.close();
         return retakePlayer;
     }
 
@@ -199,6 +226,5 @@ public class DataService {
         return new RankStats(name,
                 experience, rank, kills, deaths, shoots, hits,
                 headshots, assists, roundWin, roundLose, playtime, lastConnect);
-
     }
 }
